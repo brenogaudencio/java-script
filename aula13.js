f3e3342f5c9311ae87813220d9e04ec85cc10e81
brenogaudencio <brenobichels123@gmail.com>
2025-08-12 07:09:06 -0300

    aula14.js

--- aula13.js
+++ aula13.js
@@ -1,10 +1,20 @@
-let num1=5
-if (num1 > 11) {
-    console.log("número maior que 5")
-}else if(num1 > 5){
-    console.log("número maior que 5")
+// let num1=5
+// if (num1 > 11) {
+//     console.log("número maior que 5")
+// }else if(num1 > 5){
+//     console.log("número maior que 5")
+// }else{
+//     console.log("número menor ou igual a 5")
+// }
+
+let energia=100
+let clima="frio"
+
+if(energia > 70 && clima=="sol"){
+    console.log("Vou a praia")
 }else{
-    console.log("número menor ou igual a 5")
+    console.log("Vou ao cinema")
 }
 
-console.log("fim do programa")
+
+console.log("Fim do programa")
---
+++ aula14.js
@@ -0,0 +1,21 @@
+let colocação=10
+
+console.log("Colocação:");
+
+switch(colocação) {
+    case 1: 
+        console.log("Primeio Lugar")
+        break;
+    case 2: 
+        console.log("Segundo Lugar")
+        break;
+    case 3: 
+        console.log("Terceiro Lugar")
+        break;
+    case 4: 
+        console.log("Quarto Lugar")
+        break;    
+    default: 
+        console.log("Não subiu ao pódio")
+        break;
+}
