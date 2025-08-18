83ad88760c5cef00eefc0c9f10a9ba093c2c2b5e
brenogaudencio <brenobichels123@gmail.com>
2025-08-18 07:00:53 -0300

    aula 22, 23, 24 e 25 de js

---
+++ aula22.js
@@ -0,0 +1,23 @@
+// const valor_fixo=10
+
+// function soma(n1=valor_fixo,n2=valor_fixo){
+//     let res
+//     res = n1 * n2
+//     return res
+
+// }
+
+// let resultado_soma=soma()
+// console.log(resultado_soma)
+
+function add(v){
+    return valor+=v
+}
+let valor=0
+console.log(valor)
+
+add(10)
+console.log(valor)
+
+add(5)
+console.log(valor)  
