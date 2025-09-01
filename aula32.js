b8122e2db84abac3a73dbc07b8eebef8917d84b6
brenogaudencio <brenobichels123@gmail.com>
2025-09-01 19:34:33 -0300

    aulas js

---
+++ aula32.js
@@ -0,0 +1,8 @@
+const cursoTodos=document.getElementsByClassName("breno")
+const cursoC1=document.getElementsByClassName("c1")
+const cursoC2=document.getElementsByClassName("c2")
+
+console.log(cursoTodos)
+console.log(cursoC1)
+console.log(cursoC2)
+
